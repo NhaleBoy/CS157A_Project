34a8245dc2c4d63fcaa8f687ee6631a7f6590b47
NathanCodingJohnson <104642628+NathanCodingJohnson@users.noreply.github.com>
2024-12-09 00:40:01 -0800

    create schema

---
+++ MusicApp/src/main/java/jdbc/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS Users ( 
+                UserId INT AUTO_INCREMENT PRIMARY KEY,  
+                Username VARCHAR(100) NOT NULL UNIQUE,    
+                PrefGenre VARCHAR(100), 
+                Email VARCHAR(255) NOT NULL,  
+                Password VARCHAR(255) NOT NULL
+                );
+                
+CREATE TABLE IF NOT EXISTS Playlists ( 
+                PlaylistId INT AUTO_INCREMENT PRIMARY KEY,  
+                AuthorId INT NOT NULL,  
+                Title VARCHAR(100) NOT NULL,  
+                FOREIGN KEY (AuthorId) REFERENCES Users(UserId)  
+                );
+
+CREATE TABLE IF NOT EXISTS Audios ( 
+                AudioId INT AUTO_INCREMENT PRIMARY KEY,  
+                Title VARCHAR(100) NOT NULL,  
+                Genre VARCHAR(50) NOT NULL 
+                Category VARCHAR(50) NOT NULL 
+                AuthorId INT NOT NULL,  
+                FOREIGN KEY (AuthorId) REFERENCES Users(UserId)  +
+                );
+
+CREATE TABLE IF NOT EXISTS PlaylistContents ( 
+                PlaylistContentsId INT AUTO_INCREMENT PRIMARY KEY,  
+                PlaylistId INT NOT NULL,  
+                AudioId INT NOT NULL,  
+                FOREIGN KEY (PlaylistId) REFERENCES Playlist(PlaylistId),  
+                FOREIGN KEY (AudioId) REFERENCES Audio(AudioId) 
+                );
